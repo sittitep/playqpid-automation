39af8f0544f179b65a6b8a203b84573ea88a559d
Sittitep Tosuwan <sittitep@goeasyship.com>
2018-06-06 20:13:46 +0700

    Update skip posted posts

--- app/services/fb.rb
+++ app/services/fb.rb
@@ -16,7 +16,7 @@ class FB
     posts = FB.client.get_connections(ENV['FB_PAGE_ID'], 'posts')
     loop do
       posts.each do |post|
-        next unless post["message"].present? && post["message"].match(/#goplayqpid/)
+        next if post["message"].blank? || !post["message"].match(/#goplayqpid/) || post["message"].match(/www.playqpid.com/)
         attachments = FB.client.get_connections(post["id"], 'attachments')
 
         web_post = WebPostGenerator.new(
