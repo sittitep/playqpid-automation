ddb675d13f866234db515df71bba308985032aed
Sittitep Tosuwan <sittitep@goeasyship.com>
2018-06-14 14:41:23 +0700

    Update

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :repo_url, "https://github.com/sittitep/playqpid-automation.git"
 append :linked_files, 'config/database.yml', 'config/secrets.yml', '.env'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'web_posts', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -9,11 +9,11 @@ if (
 
   scheduler.every '2h' do
     # Delete all prev posts
-    FileUtils.rm_rf("#{Rails.root.join('web_posts')}/.", secure: true)
+    FileUtils.rm_rf("#{ENV['PLAYQPID_WEB_POST_FOLDER_PATH']}/.", secure: true)
     # Generate new posts
     FB.generate_web_post_from_page_post
     # Move new posts to web
-    FileUtils.cp_r("#{Rails.root.join('web_posts')}/.", "#{ENV['PLAYQPID_POST_FOLDER_PATH']}/")
+    FileUtils.cp_r("#{ENV['PLAYQPID_WEB_POST_FOLDER_PATH']}/.", "#{ENV['PLAYQPID_POST_FOLDER_PATH']}/")
     cmd = <<~EOF
       cd #{ENV['PLAYQPID_FOLDER_PATH']}
       git add .
